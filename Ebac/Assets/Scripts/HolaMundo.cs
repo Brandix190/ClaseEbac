28cac39b372c6a79fbe9dfc75ff753f346986bdc
Brandix190 <brandoncito54@gmail.com>
2025-11-11 18:37:25 -0600

    Hola mundo
    Se creo el primer script del proyecto llamado "Hola mundo"

---
+++ Ebac/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HolaMundo : MonoBehaviour
+{
+    void Start()
+    {
+        print("algo paso");
+        Debug.Log("Algo paso");
+        Debug.LogWarning("Algo salio medianamente mal");
+        Debug.LogError("Algo salio muy mal");
+    }
+
+    private void Update()
+    {
+        
+    }
+}
